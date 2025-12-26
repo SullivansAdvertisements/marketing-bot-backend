a4e0a114cadfe3b6946e54c198c0cccb3671e453
SullivansAdvertisements <sullysmarketing7@gmail.com>
2025-12-26 00:28:04 -0500

    Update oauth_meta.py

--- app/auth/oauth_meta.py
+++ app/auth/oauth_meta.py
@@ -1,27 +1,36 @@
-import os, requests
-from fastapi.responses import RedirectResponse
+import os
+import requests
 
 META_AUTH_URL = "https://www.facebook.com/v19.0/dialog/oauth"
 META_TOKEN_URL = "https://graph.facebook.com/v19.0/oauth/access_token"
+META_ACCOUNTS_URL = "https://graph.facebook.com/v19.0/me/adaccounts"
 
-def meta_login():
-    url = (
+
+def meta_login_url():
+    return (
         f"{META_AUTH_URL}"
         f"?client_id={os.getenv('META_APP_ID')}"
         f"&redirect_uri={os.getenv('META_REDIRECT_URI')}"
         f"&scope=ads_read,ads_management,business_management"
     )
-    return RedirectResponse(url)
 
-def meta_callback(code: str):
-    params = {
-        "client_id": os.getenv("META_APP_ID"),
-        "client_secret": os.getenv("META_APP_SECRET"),
-        "redirect_uri": os.getenv("META_REDIRECT_URI"),
-        "code": code,
-    }
 
-    response = requests.get(META_TOKEN_URL, params=params).json()
+def exchange_code_for_token(code: str):
+    r = requests.get(
+        META_TOKEN_URL,
+        params={
+            "client_id": os.getenv("META_APP_ID"),
+            "client_secret": os.getenv("META_APP_SECRET"),
+            "redirect_uri": os.getenv("META_REDIRECT_URI"),
+            "code": code,
+        },
+    )
+    return r.json()
 
-    # STORE response["access_token"] SECURELY
-    return {"platform": "meta", "token_received": True}
+
+def fetch_meta_ad_accounts(access_token: str):
+    r = requests.get(
+        META_ACCOUNTS_URL,
+        params={"access_token": access_token}
+    )
+    return r.json().get("data", [])
