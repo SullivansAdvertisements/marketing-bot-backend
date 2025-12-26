3d989132f45bd52e2db5021908f4fa7d7e0fdb82
SullivansAdvertisements <sullysmarketing7@gmail.com>
2025-12-25 23:47:10 -0500

    Update meta_ads.py

--- app/campaigns/meta_ads.py
+++ app/campaigns/meta_ads.py
@@ -1,1 +1,10 @@
-# Meta Ads API integration
+from facebook_business.api import FacebookAdsApi
+from facebook_business.adobjects.adaccount import AdAccount
+
+def create_meta_campaign(account_id, name, objective):
+    account = AdAccount(account_id)
+    return account.create_campaign(params={
+        "name": name,
+        "objective": objective,
+        "status": "PAUSED"
+    })
