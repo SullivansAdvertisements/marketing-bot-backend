7c972f75feb003a66f43c7f062831bcb29c31916
SullivansAdvertisements <sullysmarketing7@gmail.com>
2025-12-25 23:45:06 -0500

    Update meta_ad_library.py

--- app/research/meta_ad_library.py
+++ app/research/meta_ad_library.py
@@ -1,1 +1,12 @@
-# Meta Ad Library integration
+import requests
+import os
+
+def search_meta_ads(query, country):
+    url = "https://graph.facebook.com/v19.0/ads_archive"
+    params = {
+        "search_terms": query,
+        "ad_type": "ALL",
+        "ad_reached_countries": country,
+        "access_token": os.getenv("META_ACCESS_TOKEN")
+    }
+    return requests.get(url, params=params).json()
