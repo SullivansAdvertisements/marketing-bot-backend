3fb505375f62172726958a9b08274709348ed260
SullivansAdvertisements <sullysmarketing7@gmail.com>
2025-12-26 04:27:19 -0500

    Update database.py

--- app/database.py
+++ app/database.py
@@ -8,15 +8,29 @@ _engine = None
 _SessionLocal = None
 
 
+def get_database_url():
+    # Streamlit Cloud uses secrets, local dev can use env vars
+    return (
+        os.getenv("DATABASE_URL")
+    )
+
+
 def get_engine():
     global _engine
     if _engine is None:
-        _engine = create_engine(os.environ["DATABASE_URL"])
+        db_url = get_database_url()
+        if not db_url:
+            raise RuntimeError("DATABASE_URL is not set")
+        _engine = create_engine(db_url, pool_pre_ping=True)
     return _engine
 
 
 def get_session():
     global _SessionLocal
     if _SessionLocal is None:
-        _SessionLocal = sessionmaker(bind=get_engine())
+        _SessionLocal = sessionmaker(
+            autocommit=False,
+            autoflush=False,
+            bind=get_engine(),
+        )
     return _SessionLocal()
