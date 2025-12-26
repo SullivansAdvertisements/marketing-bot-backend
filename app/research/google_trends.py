1d47d1ba094ed2bd36c7857e2feb32f44dbc2bf6
SullivansAdvertisements <sullysmarketing7@gmail.com>
2025-12-25 23:44:10 -0500

    Update google_trends.py

--- app/research/google_trends.py
+++ app/research/google_trends.py
@@ -1,1 +1,6 @@
-# Google Trends integration
+from pytrends.request import TrendReq
+
+def fetch_google_trends(keyword, geo, timeframe):
+    pytrends = TrendReq(hl="en-US", tz=360)
+    pytrends.build_payload([keyword], timeframe=timeframe, geo=geo)
+    return pytrends.interest_over_time().to_dict()
