367033b367d0ac3826b731a00a1422c7550d3ccf
SullivansAdvertisements <sullysmarketing7@gmail.com>
2025-12-27 21:15:27 -0500

    Update logging.py

--- app/utils/logging.py
+++ app/utils/logging.py
@@ -1,1 +1,27 @@
-# Logging utilities
+# app/utils/logging.py
+
+import logging
+import os
+
+_LOGGER_CACHE = {}
+
+
+def get_logger(name: str) -> logging.Logger:
+    if name in _LOGGER_CACHE:
+        return _LOGGER_CACHE[name]
+
+    level = os.getenv("LOG_LEVEL", "INFO").upper()
+
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+
+    if not logger.handlers:
+        handler = logging.StreamHandler()
+        formatter = logging.Formatter(
+            "[%(asctime)s] [%(levelname)s] %(name)s: %(message)s"
+        )
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+
+    _LOGGER_CACHE[name] = logger
+    return logger
