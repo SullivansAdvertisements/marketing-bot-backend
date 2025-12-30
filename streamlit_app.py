83d97b77436e8d3dda678d48f71c8b2c64949813
SullivansAdvertisements <sullysmarketing7@gmail.com>
2025-12-29 22:19:28 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,8 @@ from oauth_google import (
     google_login_url,
     exchange_google_code_for_token,
 )
-
+from oauth_google import google_login_url
+from oauth_google import exchange_google_code_for_token
 # =================================================
 # APP TITLE
 # =================================================
