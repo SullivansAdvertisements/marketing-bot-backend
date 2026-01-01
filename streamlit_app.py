87b6a2dea38c49fd42bb09c6872b1b9105fc7859
SullivansAdvertisements <sullysmarketing7@gmail.com>
2025-12-31 23:45:47 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,7 @@
-import streamlit as st
+import sys
+import os
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 from research.router import render as research_render
 from campaigns.router import render as campaigns_render
