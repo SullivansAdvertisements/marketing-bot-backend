b9312d7564df3176f4e96df522d521609f7241d0
SullivansAdvertisements <sullysmarketing7@gmail.com>
2026-01-01 21:45:12 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ import pandas as pd
 # =========================================================
 st.set_page_config(
     page_title="Marketing Intelligence Bot",
-    layout="centered",
+    layout="
     initial_sidebar_state="collapsed",
 )
 
